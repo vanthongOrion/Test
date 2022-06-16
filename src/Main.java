da0196386d7dc72cde501c7784b37fd6d685c54d
IT21029 <vanthongvision977@gmail.com>
2022-06-16 10:22:22 +0900

    new update

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import java.io.CharConversionException;
 
 public class Main {
     public static void main(String[] args) {
-        Character dk = new Character("Dark Night", "DK", 150, new Point(100, 25, 25, 40, 10, 0));
-        Character dw = new Character("Dark Wizard", "DW", 150, new Point(100, 10, 30, 10, 20, 30));
-        Character mg = new Character("Magic Gladiator", "MG", 170, new Point(100, 15, 25, 15, 10, 35));
-        Character dl = new Character("Dark Lord", "DL", 190, new Point(100, 30, 20, 20, 10, 20));
-        Character ef = new Character("Fairy Elf", "EF", 180, new Point(100, 20, 30, 10, 10, 20));
+        Character dk = new Character("Dark Night", "DK", 150, new Point(150));
+        Character dw = new Character("Dark Wizard", "DW", 150, new Point(150));
+        Character mg = new Character("Magic Gladiator", "MG", 170, new Point(170));
+        Character dl = new Character("Dark Lord", "DL", 190, new Point(190));
+        Character ef = new Character("Fairy Elf", "EF", 180, new Point(180));
 
 
         System.out.println(dk);
